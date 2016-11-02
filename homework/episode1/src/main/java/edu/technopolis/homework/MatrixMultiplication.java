f6f2ed8bed54617a3d25f91f043ac7a076c2040b
olerom <mefiader@gmail.com>
2016-11-02 23:12:12 +0300

    Массив результата на лонге, не кидаю исключения.

--- homework/episode1/src/main/java/edu/technopolis/homework/MatrixMultiplication.java
+++ homework/episode1/src/main/java/edu/technopolis/homework/MatrixMultiplication.java
@@ -12,54 +12,66 @@ public class MatrixMultiplication {
 
     private void run(String... args) {
 
-        // Checking arguments
+        // Проверка входных аргументов
         if (args.length < 6) {
-            throw new IllegalArgumentException("Not enough arguments.");
+            System.err.println("Недостаточно минимального количеста аргументов.");
+            return;
         }
 
-        // Reading input
+        // Считывание первых четырех аргументов - размеров матриц
         int a_row, a_column, b_row, b_column;
-        a_row = Integer.parseInt(args[0]);
-        a_column = Integer.parseInt(args[1]);
+        try {
+            a_row = Integer.parseInt(args[0]);
+            a_column = Integer.parseInt(args[1]);
 
-        b_row = Integer.parseInt(args[2]);
-        b_column = Integer.parseInt(args[3]);
-
-        // Checking whether it is correct or not
-        if (a_column != b_row) {
-            throw new IllegalArgumentException("You can't multiply these matrices. Check dimensions.");
+            b_row = Integer.parseInt(args[2]);
+            b_column = Integer.parseInt(args[3]);
+        } catch (IllegalArgumentException ex) {
+            System.err.println("Ошибка: введенные аргументы не являются целочисленными.");
+            return;
         }
 
-        // Checking whether there is enough arguments
+        // Проверка матриц на согласованность
+        if (a_column != b_row) {
+            System.err.println("Невозможно перемножить матрицы: матрица несогласованы.");
+            return;
+        }
+
+        // Проверка количества аргументов
         if (args.length - 4 < a_row * a_column + b_row * b_column) {
-            throw new IllegalArgumentException("Not enough arguments.");
+            System.err.println("Недостаточно аргументов для введенных размерностей.");
+            return;
         }
 
         int a_matrix[][] = new int[a_row][a_column];
         int b_matrix[][] = new int[b_row][b_column];
         int index = 3;
 
-        // Reading first matrix
-        for (int i = 0; i < a_row; i++) {
-            for (int j = 0; j < a_column; j++) {
-                a_matrix[i][j] = Integer.parseInt(args[++index]);
+        // Считывание матриц
+        try {
+            for (int i = 0; i < a_row; i++) {
+                for (int j = 0; j < a_column; j++) {
+                    a_matrix[i][j] = Integer.parseInt(args[++index]);
+                }
             }
+
+            for (int i = 0; i < b_row; i++) {
+                for (int j = 0; j < b_column; j++) {
+                    b_matrix[i][j] = Integer.parseInt(args[++index]);
+                }
+            }
+        } catch (IllegalArgumentException ex) {
+            System.err.println("Ошибка: введенные аргументы не являются целочисленными.");
+            return;
         }
 
-        // Reading second matrix
-        for (int i = 0; i < b_row; i++) {
-            for (int j = 0; j < b_column; j++) {
-                b_matrix[i][j] = Integer.parseInt(args[++index]);
-            }
-        }
-
-        int result_matrix[][] = multiply(a_matrix, b_matrix);
+        long result_matrix[][] = multiply(a_matrix, b_matrix);
         new MatrixMultiplication().print(result_matrix);
 
     }
 
-    private int[][] multiply(int[][] a_matrix, int[][] b_matrix) {
-        int result_matrix[][] = new int[a_matrix.length][b_matrix[0].length];
+    private long[][] multiply(int[][] a_matrix, int[][] b_matrix) {
+        long result_matrix[][] = new long[a_matrix.length][b_matrix[0].length];
         for (int i = 0; i < a_matrix.length; i++) {
             for (int j = 0; j < b_matrix[0].length; j++) {
                 int multiplied_element = 0;
@@ -72,7 +84,7 @@ public class MatrixMultiplication {
         return result_matrix;
     }
 
-    private void print(int result_matrix[][]) {
+    private void print(long result_matrix[][]) {
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < result_matrix.length; i++) {
             for (int j = 0; j < result_matrix[0].length; j++) {
